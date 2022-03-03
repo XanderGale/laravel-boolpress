3e150793532aa2eab973c9bf96edd2269fdb6534
XanderGale <alessandro.galeazzi@icloud.com>
2022-03-03 02:22:41 +0100

    better user infos

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,12 +4,29 @@
     <div class="container">
         <h1>Benvenuto {{ $user->name }} !</h1>
 
+        <div class="list-group">
         @if ($user->userInfo)
-        <ul>
-            <li>Telefono: {{ $user->userInfo->phone }}</li>
-            <li>Indirizzo: {{ $user->userInfo->address }}</li>
-        </ul>
+            <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Telefono</h5>
+                </div>
+                <p class="mb-1">{{ $user->userInfo->phone }}</p>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Indirizzo</h5>
+                </div>
+                <p class="mb-1">{{ $user->userInfo->address }}</p>
+            </a>
+        @else
+            <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">No Informations</h5>
+                </div>
+                <p class="mb-1">No infos</p>
+            </a>
         @endif
+        </div>
 
     </div>
 @endsection
