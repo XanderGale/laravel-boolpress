392f8491dfd09766177539c4d55bdc65ad282a49
XanderGale <alessandro.galeazzi@icloud.com>
2022-03-06 22:22:02 +0100

    posts array from api axios

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,5 +1,28 @@
 <template>
     <section>
-        <h1>Lista post</h1>
+        <h1>Lista post:</h1>
     </section>
 </template>
+
+<script>
+export default {
+    name: 'Posts',
+    data: function() {
+        return {
+            posts: [],
+        };
+    },
+    methods: {
+        getPosts: function() {
+            // chiamata all' api per l'array posts
+            axios.get('/api/posts')
+            .then((response) =>{
+                this.posts = response.data.results;
+            });
+        }
+    },
+    created: function(){
+        this.getPosts();
+    }
+}
+</script>
